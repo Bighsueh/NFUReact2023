e7ee04439368baf470b67bf47446271f4190a4dd
Bighsueh <40841121@gm.nfu.edu.tw>
2023-04-14 02:17:18 +0800

    嘗試更改index.js

--- 第三周/first_react_project/src/index.js
+++ 第三周/first_react_project/src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const jsx_element = <p>好耶，第一個JSX!!</p>;
+root.render(jsx_element,document.getElementById('root'))
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
