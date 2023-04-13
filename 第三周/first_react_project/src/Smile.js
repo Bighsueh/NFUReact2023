92dbdc0ba5e36089c6209bb6cb919659413b40e3
Bighsueh <40841121@gm.nfu.edu.tw>
2023-04-14 02:42:17 +0800

    Compoment import/export

---
+++ 第三周/first_react_project/src/Smile.js
@@ -0,0 +1,13 @@
+//This is Smile.js
+
+import React from 'react';
+
+class Smile extends React.Component{ //Component開頭要大寫
+  render(){
+    return(
+      <h1>:)</h1>
+    )
+  }
+}
+
+export default Smile;
--- 第三周/first_react_project/src/index.js
+++ 第三周/first_react_project/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Smile from './Smile';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const jsx_element = <p>好耶，第一個JSX!!</p>;
-root.render(jsx_element,document.getElementById('root'))
+root.render(<Smile/>,document.getElementById('root'));
+
+// const jsx_element = <p>好耶，第一個JSX!!</p>;
+// root.render(jsx_element,document.getElementById('root'))
 
 
 // root.render(
